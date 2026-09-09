0eafa727ad7b26cea714cd36c5f84c9d009eccd5
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-09 10:21:36 +0300

    admin panel

--- online_magazin_core/settings.py
+++ online_magazin_core/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+# Static va Media fayllar (Rasmlar yuklash uchun)
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 
 
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'is_active')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price', 'discount_price', 'stock', 'is_available', 'is_bestseller')
+    list_filter = ('category', 'is_available', 'is_bestseller')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ProductImageInline]
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'is_main', 'created_at')
