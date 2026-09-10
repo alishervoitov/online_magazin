693b4a1f00896f73a4cb874bcf836c30006fe5ed
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-10 09:31:12 +0300

    cart item model

--- cart/models.py
+++ cart/models.py
@@ -16,3 +16,21 @@ class Cart(models.Model):
     @property
     def total_price(self):
         return sum(item.total_item_price for item in self.items.all())
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE, verbose_name="Savatcha")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Mahsulot")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Soni")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Savatcha elementi"
+        verbose_name_plural = "Savatcha elementlari"
+
+    def __str__(self):
+        return f"{self.product.title} ({self.quantity} dona)"
+
+    @property
+    def total_item_price(self):
+        price = self.product.discount_price if self.product.discount_price else self.product.price
+        return price * self.quantity
