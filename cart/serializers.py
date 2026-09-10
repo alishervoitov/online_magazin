b5a0e31e1eaf6d8da8a04cd8a8e84cb9955f6184
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-10 09:41:36 +0300

    serializer.py

---
+++ cart/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from products.models import Product
+from .models import Cart, CartItem
+from products.serializers import ProductSerializer
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product', write_only=True
+    )
+    total_item_price = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity', 'total_item_price']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price', 'created_at']
