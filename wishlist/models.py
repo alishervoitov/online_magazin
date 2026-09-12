3a1e7ce168aeb7255fa85f87843048ab84626190
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-12 09:10:18 +0300

    wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlist', verbose_name="Foydalanuvchi")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Mahsulot")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Qo'shilgan vaqti")
+
+    class Meta:
+        verbose_name = "Sevimlilar"
+        verbose_name_plural = "Sevimlilar ro'yxati"
+        unique_together = ('user', 'product') # Bitta mahsulotni ikki marta qo'shib bo'lmasligi uchun
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.title}"
