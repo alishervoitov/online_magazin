4bc90c7c59aeccebb1a08bbbf48a95e4d4608842
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-12 09:35:31 +0300

    serializers

---
+++ wishlist/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Wishlist
+from products.serializers import ProductSerializer
+from products.models import Product
+
+class WishlistSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product', write_only=True
+    )
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'product', 'product_id', 'created_at']
