d561f97de3ae1548c4a44d5b39065152c4bf8719
Alisher Voitov <voitovalisher020@gmail.com>
2026-09-15 09:37:17 +0300

    view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Review
+from .serializers import ReviewSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.all().order_by('-created_at')
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['product', 'user']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        # Faqat o'zining izohini o'zgartira oladi yoki o'chira oladi (Admin ham)
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
